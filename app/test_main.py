0310233aa504738ec7cac4773a787de377b8ea09
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-22 17:38:01 +0300

    Hell yeah solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,40 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (25, [0, 0, 0, 1])
+    ]
+)
+def test_returned_data_type(input_data: int, expected: list) -> None:
+    result = get_coin_combination(input_data)
+    assert isinstance(result, list), \
+        f"Function must return list type, not {type(result)}"
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (101, [1, 0, 0, 1])
+    ]
+)
+def test_returned_list_length(input_data: int, expected: list) -> None:
+    assert len(get_coin_combination(input_data)) == 4, \
+        "Length of list must be equal 4"
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (144, [4, 1, 1, 5])
+    ]
+)
+def test_output_data(input_data: int, expected: list) -> None:
+    assert get_coin_combination(input_data) == expected
